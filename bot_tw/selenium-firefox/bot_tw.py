6d2cedfa451eb9fd678e12c3a2872750cfdaa873
Leonardo de Jesus Queiroz <upuaut@tutanota.com>
2021-11-29 11:48:25 -0300

    Funciona clear e call menssage

--- bot_tw/selenium-firefox/bot_tw.py
+++ bot_tw/selenium-firefox/bot_tw.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox("/home/user/.mozilla/firefox/chgnbc65.default-release",executable_path="./drivers/geckodriver")
 
@@ -16,20 +17,23 @@ size = 0
 
 for i in range(100000):
     size = size+1
-    time.sleep(1)
-    elements = browser.find_elements_by_class_name('text-fragment')
+    time.sleep(0.01)
+    elements = browser.find_elements(By.CLASS_NAME,"text-fragment")
     i = len(elements)
-    print('Qt :%d' %i)
+    #print('Qt :%d' %i)
     if i > 0:
         print(elements[i-1].text)
     else:
         print('Texto: Sem texto...')
-    if size > 20:
+    if size > 200:
         size = 0
-        parei aqui...
-        button = browser.find_element('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonPrimary-sc-1qn4ixc-1 jGqsfG ksFrFH')
-
-        # clicking on the button
+        text_area = browser.find_element(By.CSS_SELECTOR,".ScInputBase-sc-1wz0osy-0")
+        text_area.send_keys("/clear")
+        button = browser.find_element(By.CSS_SELECTOR,".faqhMI > div:nth-child(1)")
         button.click()
+        #.evtFDZ
+        #.eZactg
+        #.eZactg
+        
 
 
